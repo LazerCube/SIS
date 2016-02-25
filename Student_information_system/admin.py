0342947b797f2b27a35081183143d9aa7ce10386
LazerCube <git@lazercube.com>
2016-02-25 11:18:55 +0000

    Database Update

--- Student_information_system/admin.py
+++ Student_information_system/admin.py
@@ -3,16 +3,23 @@ from .models import tblStudent, tblUnits, tblClasses
 
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('name_first', 'name_last', 'date_of_birth' ,'gender' ,'id_foreign_classes')
-    list_filter = ['gender', 'id_foreign_classes']
+    list_filter = ['gender']
     search_fields = ['name_first', 'name_last']
 
-class UnitsInLine(admin.TabularInline):
-    model = tblUnits
-    extra = 3
 
 class ClassAdmin(admin.ModelAdmin):
-    list_display = ('id_classes', 'class_code', 'id_foreign_units')
+    list_display = ('class_code', 'id_foreign_unit_1', 'id_foreign_unit_2',  'id_foreign_unit_3')
+    fieldsets = [
+        (None,          {'fields':['class_code']}),
+        ('Units',          {'fields':   ['id_foreign_unit_1',
+                                        'id_foreign_unit_2',
+                                        'id_foreign_unit_3']}),
+    ]
+
+class UnitAdmin(admin.ModelAdmin):
+    list_display = ('name_unit','description')
 
 
 admin.site.register(tblStudent, StudentAdmin)
 admin.site.register(tblClasses, ClassAdmin)
+admin.site.register(tblUnits, UnitAdmin)
--- Student_information_system/models.py
+++ Student_information_system/models.py
@@ -3,27 +3,50 @@ from django.db import models
 
 class tblUnits(models.Model):
     id_units = models.AutoField(primary_key=True)
-    name_unit = models.CharField(max_length=200)
-    description = models.CharField(max_length=300)
+    name_unit = models.CharField("Unit Name", max_length=200)
+    description = models.TextField("Description", max_length=300, null=True,
+                                    blank=False, default="None")
+
+    class Meta:
+        verbose_name = ("Unit")
+        verbose_name_plural = ("Units")
 
     def __str__(self):
         return self.name_unit
 
 class tblClasses(models.Model):
     id_classes = models.AutoField(primary_key=True)
-    class_code = models.CharField(max_length=50)
-    id_foreign_units = models.ForeignKey(tblUnits, on_delete=models.CASCADE)
+    class_code = models.CharField("Class Code", max_length=50)
+    id_foreign_unit_1 = models.ForeignKey(tblUnits, on_delete=models.CASCADE,
+                                        verbose_name="Unit 1", related_name='Unit_1',
+                                        null=True)
+    id_foreign_unit_2 = models.ForeignKey(tblUnits, on_delete=models.CASCADE,
+                                        verbose_name="Unit 2", related_name='Unit_2',
+                                        null=True)
+    id_foreign_unit_3 = models.ForeignKey(tblUnits, on_delete=models.CASCADE,
+                                        verbose_name="Unit 3", related_name='Unit_3',
+                                        null=True)
+
+    class Meta:
+        verbose_name = ("Class")
+        verbose_name_plural = ("Classes")
 
     def __str__(self):
         return self.class_code
 
 class tblStudent(models.Model):
     id_students = models.AutoField(primary_key=True)
-    name_first = models.CharField(max_length=200)
-    name_last = models.CharField(max_length=200)
-    date_of_birth = models.DateTimeField(blank=True, null=True)
-    gender = models.BooleanField()
-    id_foreign_classes = models.ForeignKey(tblClasses, on_delete=models.CASCADE)
+    name_first = models.CharField("First Name", max_length=200)
+    name_last = models.CharField("Last Name", max_length=200)
+    date_of_birth = models.DateField(blank=True, null=True)
+    gender = models.BooleanField("Is Male")
+    id_foreign_classes = models.ForeignKey(tblClasses, on_delete=models.CASCADE, verbose_name="Class")
+
+
+    class Meta:
+        verbose_name = ("Student")
+        verbose_name_plural = ("Students")
+
 
     def __str__(self):
         return ("%s %s | %s" %(self.name_first, self.name_last, self.id_foreign_classes))
