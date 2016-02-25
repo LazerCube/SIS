aa16d11d562ae42716d3cf8b84b7705fa12cb529
Elliot <elliot@lazercube.com>
2016-02-25 07:26:02 +0000

    migrate pushed

--- Student_information_system/admin.py
+++ Student_information_system/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import tblStudent, tblUnits, tblClasses
 
-# Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name_first', 'name_last', 'date_of_birth' ,'gender' ,'id_foreign_classes')
+    list_filter = ['gender', 'id_foreign_classes']
+    search_fields = ['name_first', 'name_last']
+
+class UnitsInLine(admin.TabularInline):
+    model = tblUnits
+    extra = 3
+
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ('id_classes', 'class_code', 'id_foreign_units')
+
+
+admin.site.register(tblStudent, StudentAdmin)
+admin.site.register(tblClasses, ClassAdmin)
--- Student_information_system/models.py
+++ Student_information_system/models.py
@@ -3,17 +3,27 @@ from django.db import models
 
 class tblUnits(models.Model):
     id_units = models.AutoField(primary_key=True)
+    name_unit = models.CharField(max_length=200)
     description = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name_unit
 
 class tblClasses(models.Model):
     id_classes = models.AutoField(primary_key=True)
     class_code = models.CharField(max_length=50)
     id_foreign_units = models.ForeignKey(tblUnits, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.class_code
+
 class tblStudent(models.Model):
     id_students = models.AutoField(primary_key=True)
     name_first = models.CharField(max_length=200)
     name_last = models.CharField(max_length=200)
-    date_of_birth = models.DateTimeField()
+    date_of_birth = models.DateTimeField(blank=True, null=True)
     gender = models.BooleanField()
     id_foreign_classes = models.ForeignKey(tblClasses, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return ("%s %s | %s" %(self.name_first, self.name_last, self.id_foreign_classes))
